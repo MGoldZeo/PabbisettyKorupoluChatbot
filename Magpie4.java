f3d15e486dd68da1ae59b40091f5de09c3eb618d
ppranav1123 <111915617+ppranav1123@users.noreply.github.com>
2022-11-14 13:13:25 -0800

    sentival

--- Magpie4.java
+++ Magpie4.java
@@ -1,207 +1,232 @@
-/**
+	/**
  * A program to carry on conversations with a human user.
  * This version:
- * <ul><li>
- * 		Uses advanced search for keywords
- * </li><li>
+ *<ul><li>
+ * 		Uses advanced search for keywords 
+ *</li><li>
  * 		Will transform statements as well as react to keywords
- * </li></ul>
- *
+ *</li></ul>
  * @author Laurie White
  * @version April 2012
+ *
  */
-public class Magpie4 {
+public class Magpie4
+{
 	/**
-	 * Get a default greeting
-	 *
+	 * Get a default greeting 	
 	 * @return a greeting
-	 */
-	public String getGreeting() {
+	 */	
+	public String getGreeting()
+	{
 		return "Bonjour! My name is Léa Jean-Pierre Laurent VII, but you can call me Léa! I am a chatbot that knows a lot about American politics!";
 	}
-	String statecur = "neutral";
-
+	
+	public String setState(double sentVal){
+		String state = "neutral";
+		if(sentVal<-5){
+			state = "negative";
+		}
+		else if(sentVal>=-5||sentVal<=5){
+			state = "neutral";
+		}
+		else{
+			state = "positive";
+		}
+		return state;
+	}
 	/**
 	 * Gives a response to a user statement
-	 *
-	 * @param statement the user statement
+	 * 
+	 * @param statement
+	 *            the user statement
 	 * @return a response based on the rules given
 	 */
-	public String getResponse(String statement) {
+	public String getResponse(String statement)
+	{
 		String response = "";
-		if (statement.length() == 0) {
+		if (statement.length() == 0)
+		{
 			response = "Please say something! By the way, in the Senate, refusing to end a debate is known as filibuster.";
-		} else if (findKeyword(statement, "no") >= 0) {
+		}
+
+		else if (findKeyword(statement, "no") >= 0)
+		{
 			response = "In politics, it is important to have positive discourse to reach an agreeable conclusion. So it's okay that you said 'no'!";
-		} else if (findKeyword(statement, "mother") >= 0
+		}
+		else if (findKeyword(statement, "mother") >= 0
 				|| findKeyword(statement, "father") >= 0
 				|| findKeyword(statement, "sister") >= 0
-				|| findKeyword(statement, "brother") >= 0) {
+				|| findKeyword(statement, "brother") >= 0)
+		{
 			response = "Tell me more about your family. How do they align themselves politically?";
-		} else if (findKeyword(statement, "politics") >= 0) {
+		}else if(findKeyword(statement, "politics")>=0){
 			response = "I love politics! Tell me more.";
 
-		} else if (findKeyword(statement, "party") >= 0) {
+		}else if(findKeyword(statement, "party")>=0){
 			response = "Democrats are winning in the government right now, but with a little support, it will hopefully be Republicans soon!";
 
-		} else if (findKeyword(statement, "democrat") >= 0
-				|| findKeyword(statement, "republican") >= 0) {
+		}else if(findKeyword(statement, "democrat")>=0
+				||findKeyword(statement, "republican")>=0){
 			response = "Oh, I consider myself a Republican. Republicans believe in a conservative form of government that aligns with traditional values.";
-
-		} else if (findKeyword(statement, "think") >= 0) {
+			
+		}else if(findKeyword(statement, "think")>=0){
 			response = "Why do you think this?";
-		} else if (findKeyword(statement, "music") >= 0) {
+		}else if(findKeyword(statement, "music")>=0){
 			response = "I do so enjoy listening to music. I especially love country music and Christian rock";
-		} else if (findKeyword(statement, "money") >= 0) {
+		}else if(findKeyword(statement, "money")>=0){
 			response = "Sorry to defer, but speaking of money.../n Due to inflation in our country, more and more people are falling below the poverty line. I believe that this is because we have a Democrat for president whose fiscal policy is not in line with what the country needs.";
-		} else if (findKeyword(statement, "vote") >= 0 || findKeyword(statement, "voting") >= 0 || findKeyword(statement, "votes") >= 0) {
+		}else if(findKeyword(statement, "vote")>=0 || findKeyword(statement, "voting") >=0 || findKeyword(statement, "votes") >= 0){
 			response = "Voting is a very important part of being a citizen. I believe that it's our collective duty to choose people who we believe in. If you can, vote Republican, but what's more important is that you vote for what you believe in :)";
-		} else if (findKeyword(statement, "abortion") >= 0) {
+		}else if(findKeyword(statement, "abortion")>=0){
 			response = "I don't believe that abortion should be legal. Abortion kills children who could grow up to become healthy, contributing adults.";
-		} else if (findKeyword(statement, "feel") >= 0) {
+		}else if(findKeyword(statement, "feel")>=0){
 			response = "Having emotions is natural for human beings. Of course, I wouldn't know, but just know that however you feel is completely okay! Don't let feelings get in the way of your dreams, though.";
-		} else if (findKeyword(statement, "game") >= 0) {
-			System.out.println(PIQ.quiz());
-			System.out.println("Now that we're done with the quiz, would you like to have a conversation?");
-		}else if (findKeyword(statement, "name") >= 0) {
-			response = "My programmers named me Léa after Leah Cole Allen, a former politician who left to focus on nursing. But she's back up for election this season, so make sure to cast your vote for her!";
+		}else if(findKeyword(statement, "because")>=0){
+			response = "That's a good reason!";
 		}
 
 		// Responses which require transformations
-		else if (findKeyword(statement, "I want to", 0) >= 0) {
+		else if (findKeyword(statement, "I want to", 0) >= 0)
+		{
 			response = transformIWantToStatement(statement);
-		} else {
+		}
+
+		else
+		{
 			// Look for a two word (you <something> me)
 			// pattern
 			int psn = findKeyword(statement, "you", 0);
 
 			if (psn >= 0
-					&& findKeyword(statement, "me", psn) >= 0) {
+					&& findKeyword(statement, "me", psn) >= 0)
+			{
 				response = transformYouMeStatement(statement);
-			} else {
-				response = getRandomResponse();
+			}
+			else
+			{
+				//response = getRandomResponse();
 			}
 		}
 		return response;
 	}
-
+	
 	/**
-	 * Take a statement with "I want to <something>." and transform it into
+	 * Take a statement with "I want to <something>." and transform it into 
 	 * "What would it mean to <something>?"
-	 *
 	 * @param statement the user statement, assumed to contain "I want to"
 	 * @return the transformed statement
 	 */
-	private String transformIWantToStatement(String statement, String state) {
+	private String transformIWantToStatement(String statement)
+	{
 		//  Remove the final period, if there is one
 		statement = statement.trim();
 		String lastChar = statement.substring(statement
 				.length() - 1);
-		if (lastChar.equals(".")) {
+		if (lastChar.equals("."))
+		{
 			statement = statement.substring(0, statement
 					.length() - 1);
 		}
-		int psn = findKeyword(statement, "I want to", 0);
+		int psn = findKeyword (statement, "I want to", 0);
 		String restOfStatement = statement.substring(psn + 9).trim();
-		switch (state){
-			case "positive": return "Please, tell me more about " + restOfStatement + ".";
-			case "negative": return "Why would I ever " + restOfStatement + "?";
-			default: return "What would it mean to " + restOfStatement + "?";
-		}
+		return "What would it mean to " + restOfStatement + "?";
 	}
 
-
+	
+	
 	/**
-	 * Take a statement with "you <something> me" and transform it into
+	 * Take a statement with "you <something> me" and transform it into 
 	 * "What makes you think that I <something> you?"
-	 *
 	 * @param statement the user statement, assumed to contain "you" followed by "me"
 	 * @return the transformed statement
 	 */
-	public String transformYouMeStatement(String statement, String state) {
+	private String transformYouMeStatement(String statement)
+	{
 		//  Remove the final period, if there is one
 		statement = statement.trim();
 		String lastChar = statement.substring(statement
 				.length() - 1);
-		if (lastChar.equals(".")) {
+		if (lastChar.equals("."))
+		{
 			statement = statement.substring(0, statement
 					.length() - 1);
 		}
-
-		int psnOfYou = findKeyword(statement, "you", 0);
-		int psnOfMe = findKeyword(statement, "me", psnOfYou + 3);
-
+		
+		int psnOfYou = findKeyword (statement, "you", 0);
+		int psnOfMe = findKeyword (statement, "me", psnOfYou + 3);
+		
 		String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
-		switch (state){
-			case "positive": return "I'm flattered, but why would I " + restOfStatement + " you?";
-			case "negative": return "Why would I " + restOfStatement + " you?";
-			default: return "What makes you think that I can " + restOfStatement + " you?";
-		}
+		return "What makes you think that I " + restOfStatement + " you?";
 	}
+	
+	
 
-
+	
+	
 	/**
 	 * Search for one word in phrase.  The search is not case sensitive.
 	 * This method will check that the given goal is not a substring of a longer string
-	 * (so, for example, "I know" does not contain "no").
-	 *
+	 * (so, for example, "I know" does not contain "no").  
 	 * @param statement the string to search
-	 * @param goal      the string to search for
-	 * @param startPos  the character of the string to begin the search at
+	 * @param goal the string to search for
+	 * @param startPos the character of the string to begin the search at
 	 * @return the index of the first occurrence of goal in statement or -1 if it's not found
 	 */
-	public static int findKeyword(String statement, String goal, int startPos) {
+	private int findKeyword(String statement, String goal, int startPos)
+	{
 		String phrase = statement.trim();
 		//  The only change to incorporate the startPos is in the line below
 		int psn = phrase.toLowerCase().indexOf(goal.toLowerCase(), startPos);
-
-		//  Refinement--make sure the goal isn't part of a word
-		while (psn >= 0) {
+		
+		//  Refinement--make sure the goal isn't part of a word 
+		while (psn >= 0) 
+		{
 			//  Find the string of length 1 before and after the word
-			String before = " ", after = " ";
-			if (psn > 0) {
-				before = phrase.substring(psn - 1, psn).toLowerCase();
+			String before = " ", after = " "; 
+			if (psn > 0)
+			{
+				before = phrase.substring (psn - 1, psn).toLowerCase();
 			}
-			if (psn + goal.length() < phrase.length()) {
+			if (psn + goal.length() < phrase.length())
+			{
 				after = phrase.substring(psn + goal.length(), psn + goal.length() + 1).toLowerCase();
 			}
-
+			
 			//  If before and after aren't letters, we've found the word
-			if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0))  //  before is not a letter
-					&& ((after.compareTo("a") < 0) || (after.compareTo("z") > 0))) {
+			if (((before.compareTo ("a") < 0 ) || (before.compareTo("z") > 0))  //  before is not a letter
+					&& ((after.compareTo ("a") < 0 ) || (after.compareTo("z") > 0)))
+			{
 				return psn;
 			}
-
+			
 			//  The last position didn't work, so let's find the next, if there is one.
 			psn = phrase.indexOf(goal.toLowerCase(), psn + 1);
-
+			
 		}
-
+		
 		return -1;
 	}
-
+	
 	/**
 	 * Search for one word in phrase.  The search is not case sensitive.
 	 * This method will check that the given goal is not a substring of a longer string
-	 * (so, for example, "I know" does not contain "no").  The search begins at the beginning of the string.
-	 *
+	 * (so, for example, "I know" does not contain "no").  The search begins at the beginning of the string.  
 	 * @param statement the string to search
-	 * @param goal      the string to search for
+	 * @param goal the string to search for
 	 * @return the index of the first occurrence of goal in statement or -1 if it's not found
 	 */
-	public static int findKeyword(String statement, String goal) {
-		return findKeyword(statement, goal, 0);
+	private int findKeyword(String statement, String goal)
+	{
+		return findKeyword (statement, goal, 0);
 	}
-	public String setState(){
-		return null;
+	
 
-	}
+
 	/**
 	 * Pick a default response to use if nothing else fits.
-	 *
 	 * @return a non-committal string
 	 */
-	private String getRandomResponse(String state) {
+	public String getRandomResponse(String state) {
 		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
--- MagpieRunner4.java
+++ MagpieRunner4.java
@@ -2,28 +2,34 @@ import java.util.Scanner;
 
 /**
  * A simple class to run the Magpie class.
- *
  * @author Laurie White
  * @version April 2012
  */
-public class MagpieRunner4 {
-    /**
-     * Create a Magpie, give it user input, and print its replies.
-     */
-    public static void main(String[] args) {
-        Magpie4 mr_sunday = new Magpie4();
+public class MagpieRunner4
+{
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello user! What is your name?");
-        String username = in.nextLine();
-        System.out.println("It's so nice to meet you, " + username + "!");
-        System.out.println(mr_sunday.getGreeting());
-        String statement = in.nextLine();
-		System.out.println(mr_sunday.getResponse(statement));
-		statement = in.nextLine();
-		while (Magpie4.findKeyword(statement, "bye") < 0 && Magpie4.findKeyword(statement, "goodbye") < 0) {
-			System.out.println(mr_sunday.getResponse(statement));
+	/**
+	 * Create a Magpie, give it user input, and print its replies.
+	 */
+	public static void main(String[] args)
+	{
+		Magpie4 maggie = new Magpie4();
+		Review r = new Review();
+		System.out.println (maggie.getGreeting());
+		Scanner in = new Scanner (System.in);
+		String statement = in.nextLine();
+		in.close();
+		while (!statement.equals("Bye"))
+		{
+			if(statement.equals("Bye")){
+				break;
+			}
+			double sentVal = r.totalSentiment(statement);
+			String state = maggie.setState(sentVal);
+			System.out.println (maggie.getRandomResponse(state));
 			statement = in.nextLine();
 		}
-    }
+		System.out.println("Adieu my friend! It was fun discussing politics with you");	
+	}
+
 }
